5bdb46cc2dc62e626ecaafa9e8b6bb0e949e9e83
anyelser <anyelserperez@gmail.com>
2022-06-02 13:24:28 -0400

    prueba2

--- index.php
+++ index.php
@@ -15,15 +15,15 @@
         
         <?php
         //Aqui incluimos el menu
-        echo $_SERVER["DOCUMENT_ROOT"]."/sistema_remesas_dairy/modelos/personas_modelo.php<br/>";
-        echo $_SERVER["DOCUMENT_ROOT"]."/vistas/menu.php<br/>";
-        echo getcwd(). "<br/>";
+        //echo $_SERVER["DOCUMENT_ROOT"]."/sistema_remesas_dairy/modelos/personas_modelo.php<br/>";
+        //echo $_SERVER["DOCUMENT_ROOT"]."/vistas/menu.php<br/>";
+        //echo getcwd(). "<br/>";
 
         //if()
-        echo "<br/><br/>directorio: ". __DIR__;
-        echo "<br/><br/>dirname: ". dirname(__FILE__);
-        echo "<br/><br/>archivo: ". __FILE__;
-        echo "<br/><br/>nombre directorio: ".basename(dirname(__FILE__))."<br>";
+        //echo "<br/><br/>directorio: ". __DIR__;
+        //echo "<br/><br/>dirname: ". dirname(__FILE__);
+        //echo "<br/><br/>archivo: ". __FILE__;
+        //echo "<br/><br/>nombre directorio: ".basename(dirname(__FILE__))."<br>";
         
         if($_SERVER["DOCUMENT_ROOT"] != "/app"){
 
